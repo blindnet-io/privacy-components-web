31f3b83b960a98ecac40c6f919df05c6b76b192e
Justin Boileau <jboileau@ualberta.ca>
2022-07-18 13:00:39 +0200

    Updated priv model, to be replaced by interface generated from openAPI

--- src/models/privacy-response.ts
+++ src/models/privacy-response.ts
@@ -1,7 +1,19 @@
+import { ACTION } from './priv-terms.js';
+
+// TODO: Export open API to interface and use here
+
 export interface PrivacyResponse {
   responseId: string;
   inResponseTo: string;
   date: string;
   system: string;
-  data?: object;
+  requestedAction?: ACTION;
+  dataSubject?: string[];
+  status: string;
+  motive?: string[];
+  answers?: string[];
+  message?: string;
+  lang?: string;
+  includes?: PrivacyResponse[];
+  data?: object[];
 }
