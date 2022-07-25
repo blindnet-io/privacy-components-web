e14ce6e249d15d3c41b23b2ef0491f005afedd30
jboileau99 <45305362+jboileau99@users.noreply.github.com>
2022-07-25 17:36:56 +0000

    Deploying to gh-pages from @ blindnet-io/privacy-components-web@a813a01c8b6e763ec11de49e7623f84ca3f7b598 🚀

--- dist/src/BldnPrivRequest.js
+++ dist/src/BldnPrivRequest.js
@@ -4,6 +4,7 @@ import { property, state } from 'lit/decorators.js';
 import { when } from 'lit/directives/when.js';
 import { map } from 'lit/directives/map.js';
 import { choose } from 'lit/directives/choose.js';
+import { msg } from '@lit/localize';
 import { v4 as uuidv4 } from 'uuid';
 import './DemandBuilder.js';
 import './RequestProgressIndicator.js';
@@ -130,7 +131,7 @@ export class BldnPrivRequest extends LitElement {
     render() {
         return html `
       <div id="priv-req-ctr">
-        <div class="req-hdr">My Privacy Request</div>
+        <div class="req-hdr">${msg('My Privacy Request')}</div>
         <request-progress-indicator></request-progress-indicator>
 
         <!-- BUILD AND REVIEW STATE -->
@@ -160,7 +161,7 @@ export class BldnPrivRequest extends LitElement {
                         ?disabled=${!this._buttonsClickable}
                         @click=${this.handleReviewClick}
                       >
-                        Continue to submit Privacy Request
+                        ${msg('Continue to submit Privacy Request')}
                       </button>
                     `,
             ],
@@ -172,7 +173,7 @@ export class BldnPrivRequest extends LitElement {
                         ?disabled=${!this._buttonsClickable}
                         @click=${this.handleSubmitClick}
                       >
-                        Submit Privacy Request
+                        ${msg('Submit Privacy Request')}
                       </button>
                     `,
             ],
@@ -183,16 +184,16 @@ export class BldnPrivRequest extends LitElement {
         <!-- Sent state -->
         ${when(this._requestState === RequestState.SENT, () => html `
             <strong id="req-sent-hdr" class="ctr-txt"
-              >Your privacy request has been sent!</strong
+              >${msg('Your privacy request has been sent!')}</strong
             >
-            <p class="ctr-txt">You may view the response below.</p>
+            <p class="ctr-txt">${msg('You may view the response below.')}</p>
             <button
               id="restart-btn"
               class="ctr-txt ctr-btn"
               href=""
               @click=${this.handleRestartClick}
             >
-              Submit a new Privacy Request.
+              ${msg('Submit a new Privacy Request.')}
             </button>
           `)}
       </div>
