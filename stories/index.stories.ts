b0e61f9e467a0d20d075b00ecad58991d0d81cc9
Justin Boileau <jboileau@ualberta.ca>
2022-07-26 15:08:11 +0200

    added custom locale story + docs

--- stories/index.stories.ts
+++ stories/index.stories.ts
@@ -1,5 +1,6 @@
 import { html, TemplateResult } from 'lit';
 import '../src/bldn-priv-request.js';
+import { setLocale } from '../src/utils/localization.js';
 
 export default {
   title: 'BldnPrivRequest',
@@ -9,6 +10,11 @@ export default {
       control: 'text',
       description:
         'JSON list of [actions](https://github.com/blindnet-io/product-management/blob/main/refs/schemas/priv/json-schema/priv-terms.schema.json) to include in the component.',
+    },
+    locale: {
+      control: 'select',
+      description: 'Language code for the component',
+      options: ['en', 'fr'],
     },
   },
 };
@@ -21,15 +27,26 @@ interface Story<T> {
 
 interface ArgTypes {
   actions?: string;
+  locale?: string;
 }
 
 const Template: Story<ArgTypes> = ({ actions }: ArgTypes) => html`
-  <bldn-priv-request actions="${actions || ''}" lang="fr"></bldn-priv-request>
+  <bldn-priv-request actions="${actions || ''}"></bldn-priv-request>
 `;
+
+const LocaleTemplate: Story<ArgTypes> = ({ locale }: ArgTypes) => {
+  setLocale(locale || 'fr');
+  return html` <bldn-priv-request></bldn-priv-request> `;
+};
 
 export const Regular = Template.bind({});
 
 export const CustomActions = Template.bind({});
 CustomActions.args = {
   actions: '["ACCESS","DELETE","TRANSPARENCY"]',
+};
+
+export const CustomLocale = LocaleTemplate.bind({});
+CustomLocale.args = {
+  locale: 'fr',
 };
