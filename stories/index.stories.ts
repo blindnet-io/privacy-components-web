cbe4dc0f7363f98d51eebfd8d56ff1729b8b97c3
Justin Boileau <jboileau@ualberta.ca>
2022-07-27 12:59:16 +0200

    simplified getting all locales for story

--- stories/index.stories.ts
+++ stories/index.stories.ts
@@ -1,6 +1,6 @@
 import { html, TemplateResult } from 'lit';
 import '../src/bldn-priv-request.js';
-import { sourceLocale, targetLocales } from '../src/generated/locale-codes.js';
+import { allLocales } from '../src/generated/locale-codes.js';
 import { setLocale } from '../src/utils/localization.js';
 
 export default {
@@ -15,7 +15,7 @@ export default {
     locale: {
       control: 'select',
       description: 'Language code for the component',
-      options: [sourceLocale, ...targetLocales],
+      options: allLocales,
     },
   },
 };
