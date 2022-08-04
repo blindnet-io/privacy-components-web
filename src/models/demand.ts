9c90da939c023949121633e075c383fef3173389
Justin Boileau <jboileau@ualberta.ca>
2022-08-04 12:54:20 +0200

    added restriction type

--- src/models/demand.ts
+++ src/models/demand.ts
@@ -1,7 +1,9 @@
 import { ACTION } from './priv-terms.js';
+import { Restriction } from './restriction.js';
 
 export interface Demand {
   id?: string;
   action: ACTION;
   message?: string;
+  restrictions?: Restriction[];
 }
