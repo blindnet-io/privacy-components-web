dfe7c6562dd9b3c661e2dca775a22e25bc6644fc
Justin Boileau <jboileau@ualberta.ca>
2022-08-12 07:35:10 -0600

    abstracting action form and removing dmd builder

--- packages/prci/src/demand-forms/TransparencyForm.ts
+++ packages/prci/src/demand-forms/TransparencyForm.ts
@@ -1,15 +1,18 @@
-import { html, css, LitElement } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
-import { choose } from 'lit/directives/choose.js';
 import { msg } from '@lit/localize';
-
-import '../DemandBuilderDropdownElement.js';
-import '../DemandBuilderTextElement.js';
-import { ACTION } from '../models/priv-terms.js';
+import { css, html, TemplateResult } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
 import { Demand } from '../models/demand.js';
-import { ACTION_DESCRIPTIONS } from '../utils/dictionary.js';
-import { DemandState } from '../utils/states.js';
+import { ACTION, PROVENANCE } from '../models/priv-terms.js';
+import { buttonStyles, imgStyles, textStyles } from '../styles.js';
 import { enabledActions } from '../utils/conf.js';
+import {
+  ACTION_DESCRIPTIONS,
+  PROVENANCE_DESCRIPTIONS,
+} from '../utils/dictionary.js';
+import { ActionForm } from './ActionForm.js';
+
+import '../CheckboxDropdown.js';
+import '../AllChecklist.js';
 
 /**
  * ActionForm for the Transparency PRIV action. Includes a dropdown and text element.
@@ -19,53 +22,81 @@ import { enabledActions } from '../utils/conf.js';
  * all in one DemandBuilder element.
  */
 @customElement('transparency-form')
-export class TransparencyForm extends LitElement {
-  @property({ type: Number, attribute: 'demand-state' })
-  demandState: DemandState = DemandState.EDIT;
-
+export class TransparencyForm extends ActionForm {
   @property({ type: Array, attribute: false })
   transparencyActions: ACTION[] = [];
 
-  @property({ attribute: false }) demands = new Map<string, Demand>();
+  @state() _advancedSettings = [];
 
-  @property({ attribute: false }) demandBuilderId: string = '';
+  @state() _additionalMessage = undefined;
 
-  private _extraMessage = undefined;
+  static styles = [
+    buttonStyles,
+    textStyles,
+    imgStyles,
+    css`
+      :host {
+        display: grid;
+        row-gap: 35px;
+        grid-column: 2/5;
+        align-content: flex-start;
+        border: 2px solid #000;
+        border-radius: 20px;
+        padding: 40px 60px 40px 60px;
+        margin: 0px;
+      }
+
+      :host([demand-state='2']) {
+        padding: 30px;
+        grid-column: 1/5;
+      }
+
+      #dmd-ctr {
+        display: grid;
+        row-gap: 20px;
+      }
+
+      #dmd-ctr ul {
+        margin: 0;
+      }
+
+      #dmd-ctr li:not(:last-child) {
+        margin-bottom: 15px;
+      }
+
+      #edit-heading-1 {
+        font-weight: bold;
+        grid-column: 1/2;
+        padding: 0px 0px 0px 10px;
+      }
+
+      #review-heading-1 {
+        font-weight: bold;
+        /* padding: 0px; */
+      }
+
+      #extra-msg-txt {
+        padding: 0px 0px 0px 20px;
+      }
+
+      p {
+        margin: 0px;
+      }
+    `,
+  ];
 
   constructor() {
     super();
 
     this.addEventListener('dropdown-element-add', e => {
       const details = (e as CustomEvent).detail;
-      const demandId = crypto.randomUUID();
+      // eslint-disable-next-line no-restricted-globals
+      const demandId = self.crypto.randomUUID();
       const demand: Demand = {
         action: details.id,
-        message: this._extraMessage,
+        message: this._additionalMessage,
       };
-      this.demands.set(demandId, demand);
-
-      // Fire event to set a single demand
-      const setEvent = new CustomEvent('demand-set', {
-        bubbles: true,
-        composed: true,
-        detail: {
-          demandId,
-          demand,
-        },
-      });
-      this.dispatchEvent(setEvent);
-
-      // Fire event indicating the demand builder is in a valid state once one choice is selected
-      if (details['first-selection']) {
-        const dmdValidEvent = new CustomEvent('demand-validated', {
-          bubbles: true,
-          composed: true,
-          detail: {
-            demandBuilderId: this.demandBuilderId,
-          },
-        });
-        this.dispatchEvent(dmdValidEvent);
-      }
+      this.setDemand(demandId, demand);
     });
 
     this.addEventListener('dropdown-element-delete', e => {
@@ -75,17 +106,10 @@ export class TransparencyForm extends LitElement {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         .filter(([_, d]) => d.action === details.id)
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        .forEach(([id, _]) => {
-          this.demands.delete(id);
+        .forEach(([demandId, _]) => {
+          // this.demands.delete(demandId); // NOTE: Disabling this so state flows down components
           // Fire event to delete a single demand
-          const event = new CustomEvent('demand-delete', {
-            bubbles: true,
-            composed: true,
-            detail: {
-              id,
-            },
-          });
-          this.dispatchEvent(event);
+          this.deleteDemand(demandId);
         });
 
       // Fire event indicating the demand builder for this form is in an invalid state
@@ -102,102 +126,46 @@ export class TransparencyForm extends LitElement {
     });
 
     this.addEventListener('text-element-change', e => {
-      this._extraMessage = (e as CustomEvent).detail?.text;
+      this._additionalMessage = (e as CustomEvent).detail?.text;
       // Update existing demands
       this.demands.forEach(d => {
         const demand = d;
-        demand.message = this._extraMessage;
+        demand.message = this._additionalMessage;
       });
-
-      // Fire event to set multiple demands
-      const event = new CustomEvent('demand-set-multiple', {
-        bubbles: true,
-        composed: true,
-        detail: {
-          demands: this.demands,
-        },
-      });
-      this.dispatchEvent(event);
+      this.setMultipleDemands(this.demands);
     });
   }
 
-  static styles = css`
-    :host {
-      display: grid;
-      row-gap: 35px;
-      grid-column: 2/5;
-      align-content: flex-start;
-      border: 2px solid #000;
-      border-radius: 20px;
-      padding: 40px 60px 40px 60px;
-      margin: 0px;
-    }
-
-    :host([demand-state='2']) {
-      padding: 30px;
-      grid-column: 1/5;
-    }
-
-    #dmd-ctr {
-      display: grid;
-      row-gap: 20px;
-    }
-
-    #dmd-ctr ul {
-      margin: 0;
-    }
-
-    #dmd-ctr li:not(:last-child) {
-      margin-bottom: 15px;
-    }
-
-    #edit-heading-1 {
-      font-weight: bold;
-      grid-column: 1/2;
-      padding: 0px 0px 0px 10px;
-    }
-
-    #review-heading-1 {
-      font-weight: bold;
-      /* padding: 0px; */
-    }
-
-    #extra-msg-txt {
-      padding: 0px 0px 0px 20px;
-    }
-
-    p {
-      margin: 0px;
-    }
-  `;
-
-  /**
-   * Get the edit display for a transparency demand
-   * @returns HTML template for edit display
-   */
-  getEditTemplate() {
+  getEditTemplate(): TemplateResult<1 | 2> {
     const selectedActions = Object.values(this.demands).map(d => d.action);
     return html`
       <p id="edit-heading-1">
         <b>${msg('Details of my TRANSPARENCY Demand')}</b>
       </p>
-      <demand-builder-dropdown-element
+      <checkbox-dropdown
         .choices=${this.transparencyActions.map(a => ({
           id: a,
           description: ACTION_DESCRIPTIONS[a](),
           checked: selectedActions.includes(a),
           disabled: !enabledActions.get(a) ?? true,
         }))}
-      ></demand-builder-dropdown-element>
-      <demand-builder-text-element></demand-builder-text-element>
+      ></checkbox-dropdown>
+      <slotted-dropdown header=${msg('Advanced settings')}>
+        <all-checklist
+          .choices=${Object.values(PROVENANCE).map(p => ({
+            id: p,
+            description: PROVENANCE_DESCRIPTIONS[p](),
+            checked: true,
+            disabled: false,
+          }))}
+        ></all-checklist>
+      </slotted-dropdown>
+      <slotted-dropdown header=${msg('Additional message (optional)')}>
+      </slotted-dropdown>
     `;
   }
 
-  /**
-   * Get the review display for a transparency demand
-   * @returns HTML template for review display
-   */
-  getReviewTemplate() {
+  getReviewTemplate(): TemplateResult<1 | 2> {
     return html`
       <div id="dmd-ctr">
         <p id="review-hd-1"><b>${msg('TRANSPARENCY demand')}</b></p>
@@ -208,22 +176,13 @@ export class TransparencyForm extends LitElement {
               html` <li><b>${ACTION_DESCRIPTIONS[a.action]()}</b></li> `
           )}
         </ul>
-        ${this._extraMessage
+        ${this._additionalMessage
           ? html`
               <p>${msg('Plus additional info:')}</p>
-              <p id="extra-msg-txt"><i>${this._extraMessage}</i></p>
+              <p id="extra-msg-txt"><i>${this._additionalMessage}</i></p>
             `
           : null}
       </div>
-    `;
-  }
-
-  render() {
-    return html`
-      ${choose(this.demandState, [
-        [DemandState.EDIT, () => this.getEditTemplate()],
-        [DemandState.REVIEW, () => this.getReviewTemplate()],
-      ])}
     `;
   }
 }
