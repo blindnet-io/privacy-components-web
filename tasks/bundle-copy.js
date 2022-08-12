b1b6d14904385e1ea712794094da2f5064371076
Noel Mace <contact@noelmace.com>
2022-08-13 01:49:40 +0200

    build: include all demos in gh pages
    use fs-extra instead of copyfiles to copy all files to the build
    directory to publish on GitHub pages, including builded demos

---
+++ tasks/bundle-copy.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-console */
+import fs from 'fs-extra';
+import path from 'path';
+import { __dirname } from './dirname.js';
+import { getSortedPackages } from './sorted-packages.js';
+
+const rootDir = path.resolve(__dirname(import.meta.url), '..');
+const bundleDirPath = path.resolve(rootDir, 'build');
+
+/**
+ * @param {string[]} destSubDir Path to the destination subdirectory in bundleDirPath
+ * @param {boolean} keepPkgDirName Add the package base directory name to the destination path
+ * @param {string[] | string} scope A list of globs to match the package name against
+ * @param {string[] | string} [ignore] A list of globs to filter the package name against
+ * @param {string[]} [srcSubDir] Path to the subdirectory to copy in each package
+ * @param {boolean} [showPrivate] When false, filter out private packages
+ * @returns {Promise<void[]>}
+ */
+async function copyPkgsToBundle(
+  destSubDir,
+  keepPkgDirName,
+  scope,
+  ignore,
+  srcSubDir = [],
+  showPrivate = true
+) {
+  const packages = await getSortedPackages(scope, ignore, showPrivate);
+
+  return Promise.all(
+    packages.map(pkg => {
+      const distPath = path.join(pkg.location, ...srcSubDir);
+      const pkgDirName = path.basename(pkg.location);
+      const destPath = path.join(
+        bundleDirPath,
+        ...destSubDir,
+        keepPkgDirName ? pkgDirName : ''
+      );
+
+      return fs.copy(distPath, destPath);
+    })
+  );
+}
+
+function main() {
+  // clean bundle directory
+  fs.rmSync(bundleDirPath, { recursive: true, force: true });
+
+  // run all copies in parallel
+  return Promise.all([
+    // copy builded demos
+    copyPkgsToBundle(
+      ['demos'],
+      true,
+      '@blindnet-demos/*',
+      '@blindnet-demos/static',
+      ['dist']
+    ),
+
+    // copy static demos
+    copyPkgsToBundle(['demos'], true, '@blindnet-demos/*static*'),
+
+    // copy public packages
+    copyPkgsToBundle(['packages'], true, '@blindnet/*', [], ['dist'], false),
+
+    // copy storybook build to the root of the bundle
+    fs.copy(path.resolve(rootDir, 'storybook-static'), bundleDirPath),
+  ]);
+}
+
+main().then(
+  () => {
+    console.log('all files copied');
+  },
+  reason => {
+    console.error(reason);
+    process.exit(1);
+  }
+);
--- tasks/sorted-packages.js
+++ tasks/sorted-packages.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 // @ts-ignore
 import { getPackages } from '@lerna/project';
 
@@ -9,15 +8,25 @@ import { filterPackages } from '@lerna/filter-packages';
 // @ts-ignore
 import batchPackages from '@lerna/batch-packages';
 
+import path from 'path';
+import { __dirname } from './dirname.js';
+
 /**
- * @param {string} [scope] - specific scope of the packages to build
- * @param {string} [ignore] - specific packages to ignore
+ * @param {string[] | string} [scope] A list of globs to match the package name against
+ * @param {string[] | string} [ignore] A list of globs to filter the package name against
+ * @param {boolean} [showPrivate] When false, filter out private packages
  *
  * @returns {Promise<any[]>} - sorted list of Package objects that represent packages to be built.
  */
-export async function getSortedPackages(scope = '@blindnet/*', ignore) {
-  const packages = await getPackages(__dirname);
-  const filtered = filterPackages(packages, scope, ignore, false);
+export async function getSortedPackages(
+  scope = '@blindnet/*',
+  ignore,
+  showPrivate = false
+) {
+  const packages = await getPackages(
+    path.join(__dirname(import.meta.url), '..')
+  );
+  const filtered = filterPackages(packages, scope, ignore, showPrivate);
 
   return batchPackages(filtered).reduce(
     (/** @type {string | any[]} */ arr, /** @type {any} */ batch) =>
