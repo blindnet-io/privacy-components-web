0a8588ff673e093d7aa903547e4dda37719afdb5
Justin Boileau <jboileau@ualberta.ca>
2022-08-17 18:02:21 +0200

    refactor: abstract slotted dropdown component

--- packages/prci/src/SlottedDropdown.ts
+++ packages/prci/src/SlottedDropdown.ts
@@ -1,7 +1,12 @@
 import { html, css, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { when } from 'lit/directives/when.js';
-import { buttonStyles, imgStyles, textStyles } from './styles.js';
+import {
+  buttonStyles,
+  containerStyles,
+  imgStyles,
+  textStyles,
+} from './styles.js';
 import { FormComponentState } from './utils/states.js';
 
 /**
@@ -11,23 +16,35 @@ import { FormComponentState } from './utils/states.js';
 export class SlottedDropdown extends LitElement {
   @property({ type: String }) header = '';
 
+  @property({ type: Boolean, attribute: 'include-buttons' })
+  includeButtons: Boolean = false;
+
   // Boolean indicating if the choices should be displayed initially
   @property({ type: Number, reflect: true, attribute: 'dropdown-state' })
   dropdownState: FormComponentState = FormComponentState.CLOSED;
 
   static styles = [
+    containerStyles,
     buttonStyles,
     textStyles,
     imgStyles,
     css`
       :host([dropdown-state='2']) .content-ctr {
         display: grid;
+        /* row-gap: 40px; */
       }
 
       .content-ctr {
         display: none;
-        border: 2px solid #000;
-        border-radius: 10px;
+        padding: 40px;
+      }
+
+      :host([dropdown-state='2']) .header {
+        margin: 0px 0px 20px 0px;
+      }
+
+      :host([dropdown-state='2']) .dropdown-btn-img {
+        src: 'packages/prci/src/assets/icons/close_container_arrow.svg';
       }
     `,
   ];
@@ -45,15 +62,23 @@ export class SlottedDropdown extends LitElement {
 
   render() {
     return html`
-      <button class="link-btn" @click=${this.handleButtonClick}>
-      <span class="underline">${this.header}</span>
-      <img src='/src/assets/icons/open_container_arrow.svg' alt='open-arrow' width='24' height='24'></img>
-      </button>
-      <div class="content-ctr">
+      ${when(
+        this.header,
+        () => html`
+        <button class="header link-btn" @click=${this.handleButtonClick}>
+        <span class="medium-font underline">${this.header}</span>
+        <img class="dropdown-btn-img medium-img" src='packages/prci/src/assets/icons/${
+          this.dropdownState === FormComponentState.OPEN ? 'close' : 'open'
+        }_container_arrow.svg' alt='open-arrow' width='24' height='24'></img>
+        </button>
+      `
+      )}
+      <div class="light-border content-ctr">
+        <slot name="prompt"></slot>
         <slot></slot>
         <!-- Display button top open/close if in partial/open -->
         ${when(
-          this.dropdownState !== FormComponentState.CLOSED,
+          this.includeButtons,
           () => html`
             <button
               @click=${this.handleButtonClick}
@@ -64,8 +89,6 @@ export class SlottedDropdown extends LitElement {
           `
         )}
       </div>
-
-
     `;
   }
 }
