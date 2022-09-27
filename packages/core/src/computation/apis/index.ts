858dc98aa1836f62599098bdfb0cc214d8a6d5d7
Justin Boileau <jboileau@ualberta.ca>
2022-09-27 18:02:12 +0200

    chore(@blindnet/core): new generated models and apis

---
+++ packages/core/src/computation/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CallbacksApi';
+export * from './ConfigurationApi';
+export * from './DataConsumerInterfaceApi';
+export * from './HealthApi';
+export * from './PrivacyRequestsApi';
+export * from './UserEventsApi';
+export * from './UserInfoApi';
