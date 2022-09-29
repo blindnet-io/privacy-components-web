589d2c09eb3b8912a96befbd6a72271b6f3c2787
Justin Boileau <jboileau@ualberta.ca>
2022-09-29 18:48:17 +0200

    chore(@blindndet/dci): add confirmation message after responding to demand

--- packages/core/src/components/bldn-data-consum-demand-list.ts
+++ packages/core/src/components/bldn-data-consum-demand-list.ts
@@ -2,6 +2,7 @@ import { msg } from '@lit/localize';
 import { css, html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { map } from 'lit/directives/map.js';
+import { when } from 'lit/directives/when.js';
 import { bldnStyles } from './blindnet-wc-styles.js';
 
 import './bldn-data-consum-demand-list-item.js';
@@ -13,21 +14,27 @@ export class DataConsumerDemandList extends LitElement {
 
   render() {
     return html`
-      <div id="list__row--heading">
-        <span class="list__date-col"><b>${msg('Created')}</b></span>
-        <span><b>${msg('Data Subject')}</b></span>
-        <span><b>${msg('Action')}</b></span>
-      </div>
-      <div id="list__items">
-        ${map(
-          this.demands,
-          d => html`
-            <bldn-data-consum-demand-list-item
-              demand=${JSON.stringify(d)}
-            ></bldn-data-consum-demand-list-item>
-          `
-        )}
-      </div>
+      ${when(
+        this.demands.length > 0,
+        () => html`
+          <div id="list__row--heading">
+            <span class="list__date-col"><b>${msg('Created')}</b></span>
+            <span><b>${msg('Data Subject')}</b></span>
+            <span><b>${msg('Action')}</b></span>
+          </div>
+          <div id="list__items">
+            ${map(
+              this.demands,
+              d => html`
+                <bldn-data-consum-demand-list-item
+                  demand=${JSON.stringify(d)}
+                ></bldn-data-consum-demand-list-item>
+              `
+            )}
+          </div>
+        `,
+        () => html`${msg('No requests to display.')}`
+      )}
     `;
   }
 
