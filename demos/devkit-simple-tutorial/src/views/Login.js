b68857427eff251469dc407d5afdff8b23425f08
Justin <jboileau@ualberta.ca>
2022-10-27 10:48:42 +0200

    --wip-- auth0

--- demos/devkit-simple-tutorial/src/views/Login.js
+++ demos/devkit-simple-tutorial/src/views/Login.js
@@ -1,6 +1,17 @@
 import { LitElement, html, css } from 'lit';
 
-import '@blindnet/prci'; // WTF
+import '@blindnet/prci';
+
+import { Auth0Client } from '@auth0/auth0-spa-js';
+
+const auth0 = new Auth0Client({
+  domain: 'dev-h7hjew-e.us.auth0.com',
+  client_id: '1j68DJxHXTgTj7OE3QIgiBpO7J21pVQm',
+  redirect_uri: 'http://localhost:8000/demos/devkit-simple-tutorial/privacy',
+  authorizationParams: {
+    redirect_uri: 'http://localhost:8000/demos/devkit-simple-tutorial/privacy'
+  }
+});
 
 export class AppLogin extends LitElement {
   static get properties() {
@@ -12,14 +23,18 @@ export class AppLogin extends LitElement {
   }
 
   render() {
+    auth0.getTokenSilently();
+    auth0.loginWithRedirect();
     return html`
-      <bldn-magic-auth
+
+      <!-- <bldn-magic-auth
         prompt="Please enter your email to submit a request."
         magic-pub-key="pk_live_7BFA1BB7D19A6FD9"
         redirect-uri="${window.location
           .origin}/demos/devkit-simple-tutorial/privacy"
       >
       </bldn-magic-auth>
+      <bldn-auth></bldn-auth> -->
     `;
   }
 }
--- demos/devkit-simple-tutorial/src/views/Privacy.js
+++ demos/devkit-simple-tutorial/src/views/Privacy.js
@@ -1,18 +1,26 @@
 import { LitElement, html, css } from 'lit';
 
-import '@blindnet/prci';
-
-import { Magic } from 'magic-sdk';
 import { choose } from 'lit/directives/choose.js';
+import '@blindnet/prci'
 
-const magicPublicKey = 'pk_live_7BFA1BB7D19A6FD9';
-const magic = new Magic(magicPublicKey);
+import { Auth0Client } from '@auth0/auth0-spa-js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { when } from 'lit/directives/when.js';
+
+const auth0 = new Auth0Client({
+  domain: 'dev-h7hjew-e.us.auth0.com',
+  client_id: '1j68DJxHXTgTj7OE3QIgiBpO7J21pVQm',
+  redirect_uri: 'http://localhost:8000/demos/devkit-simple-tutorial/privacy',
+  authorizationParams: {
+    redirect_uri: 'http://localhost:8000/demos/devkit-simple-tutorial/privacy'
+  }
+});
 
 export class AppPrivacy extends LitElement {
   static get properties() {
     return {
-      _isLoggedIn: { type: Boolean, state: true },
       _userData: { state: true },
+      _apiToken: { state: true },
     };
   }
 
@@ -28,85 +36,130 @@ export class AppPrivacy extends LitElement {
     window.location.href = `${window.location.origin}/demos/devkit-simple-tutorial/login`;
   }
 
-  handleLogoutClick() {
-    magic.user.logout().then(() => {
-      this._userData = false;
-    });
+  // handleLogoutClick() {
+  //   magic.user.logout().then(() => {
+  //     this._userData = false;
+  //   });
+  // }
+
+  firstUpdated() {
+
   }
 
   render() {
-    // Complete magic auth flow after redirect from login page
-    if (magic) {
-      const url = new URL(window.location.href);
-      if (url.searchParams.has('magic_credential')) {
-        magic.auth
-          .loginWithCredential()
-          .then(() => {
-            this._isLoggedIn = true;
-            // Get info on the logged in user
-            magic.user.getMetadata().then(userData => {
-              this._userData = userData;
-            });
-          })
-          .catch(e => {
-            // eslint-disable-next-line no-console
-            console.log(e);
-          });
-      } else if (this._userData === undefined) {
-        magic.user
-          .isLoggedIn()
-          .then(isLogged => {
-            if (isLogged) {
-              magic.user.getMetadata().then(userData => {
-                this._userData = userData;
-              });
-            } else {
-              this._userData = false;
-            }
-          })
-          .catch(e => {
-            // eslint-disable-next-line no-console
-            console.log(e);
-          });
-      }
-    }
 
-    // FIXME: Must configure computation-api.ts to actually use this api-token once CORS issue is resolved
+    auth0.getTokenSilently()
+      .then(token => {
+        // apiToken = token
+        auth0.getUser().then(user => {
+          console.log(user)
+          console.log('setting')
+          this._userData = user
+        })
+      })
+      .catch(() => {
+        // If not logged in, do nothing as PRCI can be used without user
+      })
+
+    console.log(this._userData)
+
     return html`
+      
       <bldn-priv-request
         data-categories='["contact", "name", "uid", "other-data"]'
-        api-token=${this._userData
-          ? this._userData.email
-          : 'john.doe@example.com'}
+        api-token=${ifDefined(this._apiToken)}
       ></bldn-priv-request>
-      <span>
-        ${choose(
-          this._userData,
-          [
-            [undefined, () => html` Getting user data... `],
-            [
-              false,
-              () => html`
-                Not logged in.<bldn-button
-                  mode="link"
-                  @bldn-button:click=${this.handleLoginClick}
-                  >Login</bldn-button
-                >
-              `,
-            ],
-          ],
-          () => html`
-            Logged in as ${this._userData.email}.
-            <bldn-button
-              mode="link"
-              @bldn-button:click=${this.handleLogoutClick}
-              >Logout</bldn-button
-            >
-          `
-        )}
-      </span>
-    `;
+      ${when(this._userData !== undefined, () => html`
+        <bldn-button
+          @click=${() => auth0.logout()}
+        >
+          Logout
+        </bldn-button>
+      `, () => html`
+        <bldn-button
+          @click=${this.handleLoginClick}
+        >
+          Login
+        </bldn-button>
+      `)}
+      
+
+    `
   }
+
+  // render() {
+  //   // Complete magic auth flow after redirect from login page
+  //   if (magic) {
+  //     const url = new URL(window.location.href);
+  //     if (url.searchParams.has('magic_credential')) {
+  //       magic.auth
+  //         .loginWithCredential()
+  //         .then(() => {
+  //           this._isLoggedIn = true;
+  //           // Get info on the logged in user
+  //           magic.user.getMetadata().then(userData => {
+  //             this._userData = userData;
+  //           });
+  //         })
+  //         .catch(e => {
+  //           // eslint-disable-next-line no-console
+  //           console.log(e);
+  //         });
+  //     } else if (this._userData === undefined) {
+  //       magic.user
+  //         .isLoggedIn()
+  //         .then(isLogged => {
+  //           if (isLogged) {
+  //             magic.user.getMetadata().then(userData => {
+  //               this._userData = userData;
+  //             });
+  //           } else {
+  //             this._userData = false;
+  //           }
+  //         })
+  //         .catch(e => {
+  //           // eslint-disable-next-line no-console
+  //           console.log(e);
+  //         });
+  //     }
+  //   }
+
+  //   // FIXME: Must configure computation-api.ts to actually use this api-token once CORS issue is resolved
+  //   return html`
+  //     <bldn-priv-request
+  //       data-categories='["contact", "name", "uid", "other-data"]'
+  //       api-token=${this._userData
+  //         ? this._userData.email
+  //         : 'john.doe@example.com'}
+  //     ></bldn-priv-request>
+  //     <span>
+  //       ${choose(
+  //         this._userData,
+  //         [
+  //           [undefined, () => html` Getting user data... `],
+  //           [
+  //             false,
+  //             () => html`
+  //               Not logged in.<bldn-button
+  //                 mode="link"
+  //                 @bldn-button:click=${this.handleLoginClick}
+  //                 >Login</bldn-button
+  //               >
+  //             `,
+  //           ],
+  //         ],
+  //         () => html`
+  //           Logged in as ${this._userData.email}.
+  //           <bldn-button
+  //             mode="link"
+  //             @bldn-button:click=${this.handleLogoutClick}
+  //             >Logout</bldn-button
+  //           >
+  //         `
+  //       )}
+  //     </span>
+  //   `;
+  // }
 }
 
 customElements.define('app-privacy', AppPrivacy);
