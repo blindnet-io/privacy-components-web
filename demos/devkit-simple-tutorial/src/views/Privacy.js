6056c590b5220f8973f619458254cbe690216f8c
Justin Boileau <jboileau@ualberta.ca>
2022-10-28 18:29:40 +0200

    chore(@blindnet/devkit-simple-tutorial): comments

--- demos/devkit-simple-tutorial/src/views/Privacy.js
+++ demos/devkit-simple-tutorial/src/views/Privacy.js
@@ -3,7 +3,7 @@ import { when } from 'lit/directives/when.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { Auth0Client } from '@auth0/auth0-spa-js';
 
-import '@blindnet/prci'
+import '@blindnet/prci';
 
 // Get an auth0 instance
 const auth0 = new Auth0Client({
@@ -11,12 +11,11 @@ const auth0 = new Auth0Client({
   client_id: '1C0uhFCpzvJAkFi4uqoq2oAWSgQicqHc',
   redirect_uri: 'http://localhost:8000/demos/devkit-simple-tutorial/privacy',
   authorizationParams: {
-    redirect_uri: 'http://localhost:8000/demos/devkit-simple-tutorial/privacy'
-  }
+    redirect_uri: 'http://localhost:8000/demos/devkit-simple-tutorial/privacy',
+  },
 });
 
 export class AppPrivacy extends LitElement {
-
   static get properties() {
     return {
       _userData: { state: true },
@@ -32,21 +31,23 @@ export class AppPrivacy extends LitElement {
     `;
   }
 
+  /**
+   * Get a blindnet token given an auth0 ones
+   * @param {string} auth0Token
+   * @returns Promise<Response>
+   */
   async getBlindnetToken(auth0Token) {
-
     const headers = {
-      Authorization: `Bearer ${auth0Token}`
-    }
+      Authorization: `Bearer ${auth0Token}`,
+    };
 
     return fetch(
-      // 'https://blindnet-connector-demo-staging.azurewebsites.net/auth/token',
-      'https://devkit-pce-staging.azurewebsites.net/v0/auth/token',
+      'https://blindnet-connector-demo-staging.azurewebsites.net/auth/token',
       {
         method: 'GET',
-        headers
+        headers,
       }
-    )
-
+    );
   }
 
   handleLoginClick() {
@@ -54,54 +55,50 @@ export class AppPrivacy extends LitElement {
   }
 
   handleLogoutClick() {
-    auth0.logout()
+    auth0.logout();
   }
 
   render() {
-
-    // Try to get a login token
-    auth0.getTokenSilently()
-      .then(token => {
-        this._apiToken = token;
-        this.getBlindnetToken().then(result => {
-          console.log(result)
-        })
+    // Try to get an auth0 token
+    auth0
+      .getTokenSilently()
+      .then(auth0Token => {
+        // Get a blindnet token with our auth0 one
+        this.getBlindnetToken(auth0Token).then(blindnetToken => {
+          // Use this token for the PRCI
+          this._apiToken = blindnetToken;
+        });
+        // Get user info
         auth0.getUser().then(user => {
-          this._userData = user
-        })
+          this._userData = user;
+        });
       })
       .catch(() => {
-        this._apiToken = undefined
         // If not logged in, do nothing as PRCI can be used unauthenticated
-      })
-
+        this._apiToken = undefined;
+      });
 
     return html`
-      
       <bldn-priv-request
         data-categories='["contact", "name", "uid", "other-data"]'
         api-token=${ifDefined(this._apiToken)}
       ></bldn-priv-request>
 
-      ${when(this._userData, () => html`
-      
-        <span>Logged in as ${this._userData?.email}.</span>
-        <bldn-button
-          @click=${this.handleLogoutClick}
-          mode='link'
-        >
-          Logout
-        </bldn-button>
-
-      `, () => html`
-        <bldn-button
-          @click=${this.handleLoginClick}
-          mode='link'
-        >
-          Login
-        </bldn-button>
-      `)}
-    `
+      ${when(
+        this._userData,
+        () => html`
+          <span>Logged in as ${this._userData?.email}.</span>
+          <bldn-button @click=${this.handleLogoutClick} mode="link">
+            Logout
+          </bldn-button>
+        `,
+        () => html`
+          <bldn-button @click=${this.handleLoginClick} mode="link">
+            Login
+          </bldn-button>
+        `
+      )}
+    `;
   }
 }
 
