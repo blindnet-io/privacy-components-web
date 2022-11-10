50891cf60507372d574cebb8e7836f101d774308
Justin Boileau <jboileau@ualberta.ca>
2022-11-10 11:44:48 +0100

    fix(@blindnet/prci): fix local path and version

--- packages/prci/src/utils/localization.ts
+++ packages/prci/src/utils/localization.ts
@@ -10,5 +10,5 @@ import { sourceLocale, targetLocales } from '../generated/locale-codes.js';
 export const { getLocale, setLocale } = configureLocalization({
   sourceLocale,
   targetLocales,
-  loadLocale: (locale: string) => import(`../generated/locales/${locale}.js`),
+  loadLocale: (locale: string) => import(`./generated/locales/${locale}.js`),
 });
